14018286b8ddd7543832d65460a2713c70ca0209
Nethli Dahanayake <asleeish20002gmail.com>
2022-01-22 09:15:35 +0530

    displaying who logged in

--- HTML/mainPage.php
+++ HTML/mainPage.php
@@ -1,26 +1,27 @@
 <?php
     include '../PHP/server.php';
     include 'LoginValidation.php';
-    include 'loginForm.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main Page</title>
 
+    <!--Linking CSS External Files-->
     <link rel = "stylesheet" href = "../CSS/MainPageStyling.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+    
+    <!--Linking Bootstrap External Files-->
+    <link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    <!--<script>
-      function getUser(){
-        document.getElementById('user').innerHTML = "Hi";
-      }
-    </script>-->
+    <!--Linking JS External Files-->
+   <script type="text/javascript" src = 'https://kit.fontawesome.com/a076d05399.js'></script>
+
 
 </head>
 
@@ -28,7 +29,7 @@
 <!--Navigation Bar-->
     <div class="collapse" id="navbarToggleExternalContent">
         <div class="bg-dark p-4">
-          <h5 class="text-white h4">Welcome,</h5>
+          <h5 class="text-white h4">Welcome <?php echo ucfirst($_SESSION['FirstName']) ."!!";?></h5>
           <span class="text-muted">For a safe ride ...</span>
         </div>
     </div>
@@ -39,8 +40,7 @@
             <span class="navbar-toggler-icon"></span>
           </button>
           
-          <p id ="user"  align = "right"><font color="white">Welcome User</font></p>
-
+          <span class="fas fa-user-alt"><p align = "right"><font color="white"><?php echo ($_SESSION['FirstName']);?></font></p></span>
         </div>
     </nav>
 
@@ -61,16 +61,14 @@
             <li><a href = "loginForm.php">About</a></li>
             <li><a href = "loginForm.php">News</a></li>
             <li><a href = "loginForm.php">Home</a></li>
-            <li><a href = "loginForm.php">About</a></li>
+            <li><a href = "loginForm.php">Logout</a></li>
             <li><a href = "loginForm.php">News</a></li>
             <li><a href = "loginForm.php">Sign up</a></li>        
     </ul>    
-
+<!--Define Footer-->
    <footer>
             <p>&copy; 2022 Baymax Transportaion Service</p>
    </footer>
-
-
 
 </body>
 </html>
